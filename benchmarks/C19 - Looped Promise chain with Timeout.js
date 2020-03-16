2f92535a1a098a1f96c49102b54c96a4488684f3
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-03-16 22:37:15 +0000

    Remove access to `window`.

--- benchmarks/C19 - Looped Promise chain with Timeout.js
+++ benchmarks/C19 - Looped Promise chain with Timeout.js
@@ -3,7 +3,7 @@
  */
 [ 1, 2, 3, 4, 5 ].reduce( ( chain, i ) => {
 	return chain.then( () => new Promise( r => {
-		window.setTimeout( () => {
+		setTimeout( () => {
 			console.log( i );
 			r();
 		}, 1000 );
