7a09241293a9d2868ec114c3028b87cf9f8e55ca
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-03-18 16:49:21 +0000

    Add catch registration scheduled by Promise.

--- benchmarks/C41 - Rejection handled asynchronously (microtask).js
+++ benchmarks/C41 - Rejection handled asynchronously (microtask).js
@@ -13,6 +13,8 @@ const ago = Date.now();
 
 while ( Date.now() - ago < 2000 );
 
+Promise.resolve().then( () => p1.catch( v => console.warn( 'promise:', v ) ) );
+
 queueMicrotask( () => p1.catch( v => console.warn( 'queueMicrotask:', v ) ) );
 
 process.nextTick( () => p1.catch( v => console.warn( 'nextTick:', v ) ) );
