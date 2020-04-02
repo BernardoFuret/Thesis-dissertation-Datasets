add241b292bee3dd652b8fc9a2a64a975b552c96
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-04-02 15:28:28 +0100

    Cleaup and stick to a simple case.

--- benchmarks/O13 - Unexpected Execution Order (simple).js
+++ benchmarks/O13 - Unexpected Execution Order (simple).js
@@ -1,4 +1,4 @@
-/*function doThrow( msg ) {
+function doThrow( msg ) {
 	throw new Error( msg );
 }
 
@@ -15,39 +15,6 @@ var manager = new class Manager {
 			? console.log( 'Success!' )
 			: doThrow( 'Different!' )
 		;
-	}
-}
-
-var str = 'str';
-
-manager.create( str, str => {
-//	manager.validateStr( str );
-} );
-
-manager.validateStr( str );*/
-
-// ---------------------------------------
-
-function doThrow( msg ) {
-	throw new Error( msg );
-}
-
-var manager = new class Manager {
-	create( str, onCreate ) {
-		return new Promise( r => {
-			setTimeout( () => {
-				r( this.str = str );
-			}, Math.random() * 1000 )
-		} ).then( onCreate );
-	}
-
-	validateStr( str ) {
-		return new Promise( r => {
-			setTimeout( () => {
-				r( str === this.str || doThrow( 'Different!' ) );
-			}, Math.random() * 1000 )
-		} )
-			.then( () => console.log( 'Success!' ) );
 	}
 }
 
