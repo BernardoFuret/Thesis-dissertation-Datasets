97542eb4fcd7ebd7378ddf9715ac537fc22c073f
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-10-05 13:07:43 +0100

    Convert indentation to tabs.

--- notable cases/Broken Promise/I07.js
+++ notable cases/Broken Promise/I07.js
@@ -2,15 +2,15 @@
  * Based on https://github.com/nuprl/PromiseKeeper/blob/9d244db90315b467e80fd3240896496d5d7526d8/tests-unit/input/BrokenPromiseChain.js
  */
 function f () {
-    var p = new Promise(function (resolve, reject) {
-        resolve( 'world' );
-    });
-    p.then(function addHello(v) {
-        return 'hello ' + v;
-    });
-    return p;
+	var p = new Promise(function (resolve, reject) {
+		resolve( 'world' );
+	});
+	p.then(function addHello(v) {
+		return 'hello ' + v;
+	});
+	return p;
 }
 var p2 = f();
 p2.then(function finalize(x) {
-    console.log(x);
+	console.log(x);
 });
